79919dd3ebe7bb349d929ebe8022ce7ff13e8bb4
Takuchan <awatykhall850@outlook.com>
2020-03-21 22:43:45 +0900

    プルダウン・if文で分岐までやった

--- app/src/main/java/com/takuchan/paymoney/MainActivity.java
+++ app/src/main/java/com/takuchan/paymoney/MainActivity.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +26,18 @@ public class MainActivity extends AppCompatActivity{
     TextView onepay,payof,paypay,extra;
     ListView listView;
     ArrayAdapter<String> adapter;
+    Spinner spinner;
+
+    String[] dropItem = {
+            "均等に割る",
+            "10¥まで",
+            "100¥まで",
+            "1,000¥まで",
+            "10,000¥まで",
+            "この機能はなんですか"
+    };
+    String itemname;
+    ImageView pulldownimage;
 
     int totalprice[] = new int[1000];
     int i = 1;
@@ -31,6 +46,8 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pulldownimage = new ImageView(this);
+        pulldownimage.setImageResource(R.drawable.pulldown);
 
         people = (EditText)findViewById(R.id.editText); price = (EditText)findViewById(R.id.editText2);
         add = (Button)findViewById(R.id.button2); finalbutton = (Button)findViewById(R.id.button4);
@@ -38,6 +55,30 @@ public class MainActivity extends AppCompatActivity{
         listView = (ListView)findViewById(R.id.listVIew);
         adapter  = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1);
         listView.setAdapter(adapter);
+        //スピナーのドロップ
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter spinneradapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,dropItem);
+        spinner.setAdapter(spinneradapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                itemname = (String) parent.getAdapter().getItem(position);
+                if(itemname.equals("この機能はなんですか")){
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("この機能はなんですか")
+                            .setMessage("例えば、3人で2000¥の商品を割り勘するとします。\n2000÷3\n普通に計算すると666¥あまり6666...と続きます\n\n" +
+                                    "この場合に「10¥まで」を選ぶと\n一人以外は、一人あたりの支払価格は660¥となり1の位は四捨五入されます。\n四捨五入した1の位の6¥は、友達間で順番に交代してに支払っていくことや、幹事支払ってもらうなど、一人で全員分の四捨五入された価格を仕払ます。そして小銭を出し合う必要がなくなります。")
+                            .setPositiveButton("OK", null)
+                            .show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +94,7 @@ public class MainActivity extends AppCompatActivity{
                 }
             }
         });
+
         finalbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,18 +111,44 @@ public class MainActivity extends AppCompatActivity{
                             .setPositiveButton("OK", null)
                             .show();
                 }else if(!(people.getText().length() == 0 && ok == 0)){
-                    int peopleint = Integer.parseInt(people.getText().toString());
-                    Toast.makeText(MainActivity.this, "計算が終わりました", Toast.LENGTH_SHORT).show();
-                    int num = 0;
-                    for (int i = 0;i<totalprice.length; i ++){
-                        num += totalprice[i];
-                    }
-                    onepay.setText(num / peopleint + "¥");
-                    extra.setText("あまりは"+num%peopleint+"¥");
+                    ItemSelectDetail();
                 }
             }
         });
 
+    }
+    void ItemSelectDetail(){
+        int peopleint = Integer.parseInt(people.getText().toString());
+        int num = 0;
+        for (int i = 0;i<totalprice.length; i ++){
+            num += totalprice[i];
+        }
+        if(itemname.equals("この機能はなんですか")){
+            new AlertDialog.Builder(this)
+                    .setTitle("確認")
+                    .setMessage("OKボタンを押して、何円で切り上げるかを選択してください。")
+                    .setView(pulldownimage)
+                    .setPositiveButton("OK",null)
+                    .show();
+        }else if(itemname.equals("10¥まで")){
+            int  answer = num/peopleint / 10 * 10;
+            onepay.setText(answer + "¥");
+            extra.setVisibility(View.VISIBLE);
+            extra.setText(num/peopleint % 10 + "¥");
+
+
+        }else if(itemname.equals("100¥まで")){
+
+        }else if(itemname.equals("1,000¥まで")){
+
+        }else if(itemname.equals("10,000まで")){
+
+        }else if(itemname.equals("均等に割る")){
+            onepay.setText(num / peopleint + "¥");
+            extra.setVisibility(View.VISIBLE);
+            extra.setText("あまりは"+num%peopleint+"¥");
+            Toast.makeText(this, "計算が終わりました", Toast.LENGTH_SHORT).show();
+        }
     }
 
 

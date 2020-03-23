8db734d68fb1645a1faec467665a03c0265ff6cd
Takuchan <awatykhall850@outlook.com>
2020-03-24 00:09:34 +0900

    レイアウトと四捨五入を完全実装した

--- app/src/main/java/com/takuchan/paymoney/MainActivity.java
+++ app/src/main/java/com/takuchan/paymoney/MainActivity.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity{
     EditText people,price;
     Button add,finalbutton;
-    TextView onepay,payof,paypay,extra;
+    TextView onepay,totalnum,extra;
     ListView listView;
     ArrayAdapter<String> adapter;
     Spinner spinner;
@@ -31,9 +31,6 @@ public class MainActivity extends AppCompatActivity{
     String[] dropItem = {
             "均等に割る",
             "10¥まで",
-            "100¥まで",
-            "1,000¥まで",
-            "10,000¥まで",
             "この機能はなんですか"
     };
     String itemname;
@@ -51,7 +48,7 @@ public class MainActivity extends AppCompatActivity{
 
         people = (EditText)findViewById(R.id.editText); price = (EditText)findViewById(R.id.editText2);
         add = (Button)findViewById(R.id.button2); finalbutton = (Button)findViewById(R.id.button4);
-        onepay = (TextView)findViewById(R.id.textView7); payof = (TextView)findViewById(R.id.textView9); paypay = (TextView)findViewById(R.id.textView11); extra = (TextView)findViewById(R.id.textView);
+        onepay = (TextView)findViewById(R.id.textView7); totalnum = (TextView)findViewById(R.id.textView8); extra = (TextView)findViewById(R.id.textView);
         listView = (ListView)findViewById(R.id.listVIew);
         adapter  = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1);
         listView.setAdapter(adapter);
@@ -66,8 +63,9 @@ public class MainActivity extends AppCompatActivity{
                 if(itemname.equals("この機能はなんですか")){
                     new AlertDialog.Builder(MainActivity.this)
                             .setTitle("この機能はなんですか")
-                            .setMessage("例えば、3人で2000¥の商品を割り勘するとします。\n2000÷3\n普通に計算すると666¥あまり6666...と続きます\n\n" +
-                                    "この場合に「10¥まで」を選ぶと\n一人以外は、一人あたりの支払価格は660¥となり1の位は四捨五入されます。\n四捨五入した1の位の6¥は、友達間で順番に交代してに支払っていくことや、幹事支払ってもらうなど、一人で全員分の四捨五入された価格を仕払ます。そして小銭を出し合う必要がなくなります。")
+                            .setMessage("例えば、3人で2000¥の商品を割り勘するとします。\n2000÷3=666・・66...\nあまりが出てしまいました。\n\n" +
+                                    "この場合に「10¥まで」を選ぶと\n1の位は四捨五入され、一人あたりの支払価格は670¥となります。\nこれにより１円玉を出し合う必要がなくなります。\n上の例はおつりが帰ってくる計算です。ですがおつりが帰らず、逆に多く支払う" +
+                                    "場合もあります。例えば１の位が3で四捨五入して０になる場合です。この場合では一人だけが多く支払うことになるので、平等を保つために友達間や仲間で順番に交代して支払っていくことが大切です。金銭のやり取りなのでしっかりと話し合いで約束を決めましょう。")
                             .setPositiveButton("OK", null)
                             .show();
                 }
@@ -88,6 +86,7 @@ public class MainActivity extends AppCompatActivity{
                     i = i + 1;
                     ok = 1;
                     price.setText(null);
+
                 }else{
                     Toast.makeText(MainActivity.this, "料金を入力してね", Toast.LENGTH_SHORT).show();
 
@@ -123,6 +122,7 @@ public class MainActivity extends AppCompatActivity{
         for (int i = 0;i<totalprice.length; i ++){
             num += totalprice[i];
         }
+        totalnum.setText("計:"+ num  + "¥");
         if(itemname.equals("この機能はなんですか")){
             new AlertDialog.Builder(this)
                     .setTitle("確認")
@@ -132,16 +132,17 @@ public class MainActivity extends AppCompatActivity{
                     .show();
         }else if(itemname.equals("10¥まで")){
             int  answer = num/peopleint / 10 * 10;
+            if((num/peopleint % 10) > 5) {
+                answer = answer + 10;
+            }
             onepay.setText(answer + "¥");
             extra.setVisibility(View.VISIBLE);
-            extra.setText(num/peopleint % 10 + "¥");
-
-
-        }else if(itemname.equals("100¥まで")){
-
-        }else if(itemname.equals("1,000¥まで")){
-
-        }else if(itemname.equals("10,000まで")){
+            int otsuri = num - answer * peopleint;
+            if (otsuri < 0){
+                extra.setText("おつり"+otsuri +  "¥");
+            }else{
+                extra.setText("一人だけ+" + otsuri+"¥を払ってください");
+            }
 
         }else if(itemname.equals("均等に割る")){
             onepay.setText(num / peopleint + "¥");
